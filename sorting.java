a785c64c2840e5e5aaa1a7226aef9a330f11347a
mehmetsaidozsoy <mehmetsaidozsoy@gmail.com>
2023-05-13 21:54:48 +0300

    Sıralama Uygulaması

---
+++ sorting.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class sorting {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("İlk sayıyı girin :");
+        a = input.nextInt();
+
+        System.out.println("İkinci sayıyı girin :");
+        b = input.nextInt();
+
+        System.out.println("Üçüncü sayıyı girin :");
+        c = input.nextInt();
+
+        if (a > b && a > c) {
+            if (b > c) {
+                System.out.println("a>b>c");
+            } else {
+                System.out.println("a>c>b");
+            }
+        } else {
+            if (b > a && b > c) {
+                if (a > c) {
+                    System.out.println("b>a>c");
+                } else {
+                    System.out.println("b>c>a");
+                }
+
+            }
+        }
+        if(c>a&&c>b){
+            if(a>b){
+                System.out.println("c>a>b");
+            }
+            else {System.out.println("c>b>a");}
+        }
+
+    }
+}
